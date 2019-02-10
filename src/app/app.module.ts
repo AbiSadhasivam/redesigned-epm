ccc82bb48ef75c92d539fe855de0b5c155d49d85
Suriya Prashath <suriya.sadaiyappan@solitontech.com>
2019-02-10 18:49:26 +0530

    Add basic routes for the app
    Signed-off-by: Suriya Prashath <suriya.sadaiyappan@solitontech.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { UsersComponent } from "./users/users.component";
+import { UsersComponent } from "./main/users/users.component";
+import { MainComponent } from './main/main.component';
+import { ShiftComponent } from './main/shift/shift.component';
+import { ReportComponent } from './main/report/report.component';
 
 const routes: Routes = [
   {
@@ -12,13 +15,25 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: "users",
+    path: "epm",
+    component: MainComponent
+  },
+  {
+    path: "epm/users",
     component: UsersComponent
+  },
+  {
+    path: "epm/shifts",
+    component: ShiftComponent
+  },
+  {
+    path: "epm/reports",
+    component: ReportComponent
   }
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, UsersComponent],
+  declarations: [AppComponent, HomeComponent, UsersComponent, MainComponent, ShiftComponent, ReportComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
